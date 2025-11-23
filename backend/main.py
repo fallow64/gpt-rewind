f9a92aae506b578d17e7ea73fc6945a0db8f30b3
parallax9999 <geetygam@proton.me>
2025-11-22 18:29:03 -0600

    initial scaffolding of fastAPI (main.py)

--- backend/main.py
+++ backend/main.py
@@ -7,4 +7,25 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
+@app.get("/user/{userID}/insights")
 
+
+
+@app.get("/user/{userID}/sound/{index}")
+
+
+@app.post("/user/{userID}/conversation")
+
+
+
+
+
+
+
+
+
+# GET /user/userId/insights -> JSON results
+# GET /user/userId/sound/index -> Sound for slide index=
+# POST /user/userId/conversation -> upload file to process
+#
+#
