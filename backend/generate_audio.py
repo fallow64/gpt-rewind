740849a7bc33d763e63ede3bb4e36b27cc06dd70
parallax9999 <geetygam@proton.me>
2025-11-22 20:10:27 -0600

    rewrote transcribe to only generate 1 audio file at a time

--- backend/generate_audio.py
+++ backend/generate_audio.py
@@ -5,18 +5,13 @@ from fishaudio import FishAudio
 from fishaudio.utils import save
 
 
-
-
-
-async def transcribe(userID: str, insights: list[str]):
+async def transcribe(userID: str, insight: str, insightID: int):
     client = FishAudio(api_key=FISH_AUDIO_API_KEY)
-    i = 1
-    for text in insights:
-        audio = client.tts.convert(text = text, reference_id = REFERENCE_ID)
-        # Ensure the directory exists before saving
-        output_dir = os.path.join(os.getcwd(), "user", userID, "sound")
-        os.makedirs(output_dir, exist_ok=True)
-        save(audio, os.path.join(output_dir, f"{i}.mp3"))
+    audio = client.tts.convert(text = insight, reference_id = REFERENCE_ID)
+    output_dir = os.path.join(os.getcwd(), "user", userID, "sound")
+    os.makedirs(output_dir, exist_ok=True)
+    save(audio, os.path.join(output_dir, f"{insightID}.mp3"))
+
 
 
 
